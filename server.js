63579490cb2e08e83a0beb6457f3841ae130cde9
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-06-15 15:34:09 +0200

    [Feature] Deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const items = require('./server/routes/api/item');
 
 const app = express();
@@ -7,6 +8,16 @@ const app = express();
 app.use(express.json());
 app.use(morgan('short'));
 app.use('/api/items', items);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    // For any other url reload index page
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
